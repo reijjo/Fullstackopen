7a6fa26a31344e88ff29f4daa93de709a218a6b3
reijjo <reijjo@reijjos-Air.home>
2023-03-09 21:20:50 +0200

    eka tehtava tehty osa2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios';
+import personService from './services/persons';
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm';
-import Persons from './components/Persons';;
+import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -12,10 +12,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -30,7 +30,11 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
-      setPersons(persons.concat(nameObject))
+			personService
+			.create(nameObject)
+			.then(returnedPerson => {
+				setPersons(persons.concat(returnedPerson))
+			})
     }
       setNewName('')
       setNewNumber('')
