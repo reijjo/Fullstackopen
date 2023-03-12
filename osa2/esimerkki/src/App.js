e9da37882830bad2fe393e668ef132dcb2381aec
reijjo <reijjo@reijjos-Air.home>
2023-03-12 10:16:34 +0200

    esim part2

--- osa2/esimerkki/src/App.js
+++ osa2/esimerkki/src/App.js
@@ -1,11 +1,28 @@
 import { useState, useEffect } from 'react';
 import Note from './components/Note';
+import Notification from './components/Notification';
 import noteService from './services/notes'
 
+const Footer = () => {
+	const footerStyle = {
+		color: 'green',
+		fontStyle: 'italic',
+		fontSize: 16
+	}
+
+	return (
+		<div style={footerStyle}>
+			<br />
+			<em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+		</div>
+	)
+}
+
 const App = () => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(null)
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState('some error happened...');
 
   useEffect(() => {
     noteService
@@ -15,6 +32,10 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+	if (!notes) {
+		return null
+	}
 
   const addNote = (event) => {
     event.preventDefault(); // Ei lataa sivuu uusiks
@@ -50,9 +71,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already deleted from server`
       )
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -60,6 +84,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+			<Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -81,6 +106,7 @@ const App = () => {
         />
         <button type='submit'>save</button>
       </form>
+			<Footer />
     </div>
   )
 }
