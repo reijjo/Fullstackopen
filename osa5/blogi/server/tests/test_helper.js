299002e25b6143ea47c333ad8f06b1daa11cee5a
reijjo <reijjo@reijjos-Air.home>
2023-04-06 17:57:56 +0300

    aaa

--- osa5/blogi/server/tests/test_helper.js
+++ osa5/blogi/server/tests/test_helper.js
@@ -1,11 +1,13 @@
-const Blog = require('../models/blog')
-const User = require('../models/user')
+/* eslint-disable semi */
+/* eslint-disable quotes */
+// const Blog = require('../models/blog')
+const User = require("../models/user");
 
 const usersInDb = async () => {
-	const users = await User.find({})
-	return users.map(u => u.toJSON())
-}
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
 
 module.exports = {
-	usersInDb
-}
+  usersInDb,
+};
