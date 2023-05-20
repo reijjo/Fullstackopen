4ee30651baac977a4ed1b81cd564f13538a0b845
reijjo <teemu.aitomeri@gmail.com>
2023-05-20 13:16:33 +0300

    part8d skipped

--- osa8/apollo/client/src/requests.js
+++ osa8/apollo/client/src/requests.js
@@ -29,18 +29,25 @@ export const ALL_PERSONS = gql`
   }
 `;
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+`;
+
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `;
 
 export const EDIT_NUMBER = gql`
