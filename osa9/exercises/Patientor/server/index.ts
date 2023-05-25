481442276fe42686dd47f48e88991e9cf18b5b05
reijjo <teemu.aitomeri@gmail.com>
2023-05-25 19:17:49 +0300

    9.9 done

--- osa9/exercises/Patientor/server/index.ts
+++ osa9/exercises/Patientor/server/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
