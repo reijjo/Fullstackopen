78a9f5f6fe81ca271d704d5a0d2c967ebf41153a
reijjo <teemu.aitomeri@gmail.com>
2023-05-31 19:53:01 +0300

    9-17 done

--- osa9/example/ilari/flight-front/src/App.tsx
+++ osa9/example/ilari/flight-front/src/App.tsx
@@ -4,7 +4,10 @@ import diaryService from "./diaryService";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [newDiary, setNewDiary] = useState("");
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
   const [diaries, setDiaries] = useState<Diary[]>([
     // {
     //   id: 1,
@@ -22,9 +25,58 @@ const App = () => {
     });
   }, []);
 
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    diaryService
+      .createDiary({
+        date: date,
+        visibility: visibility,
+        weather: weather,
+        comment: comment,
+      })
+      .then((data) => {
+        setDiaries(diaries.concat(data));
+      });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
+
   return (
     <>
       <h2>Add new entry</h2>
+      <form onSubmit={diaryCreation}>
+        <div>
+          date{" "}
+          <input
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility{" "}
+          <input
+            value={visibility}
+            onChange={(event) => setVisibility(event.target.value)}
+          />
+        </div>
+        <div>
+          weather{" "}
+          <input
+            value={weather}
+            onChange={(event) => setWeather(event.target.value)}
+          />
+        </div>
+        <div>
+          comment{" "}
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
 
       <h2>Diary entries</h2>
 
@@ -33,6 +85,7 @@ const App = () => {
           <h2>{trip.date}</h2>
           <div>visibility: {trip.visibility}</div>
           <div>weather: {trip.weather}</div>
+          {trip.comment ? <div>- {trip.comment}</div> : null}
         </div>
       ))}
     </>
--- osa9/example/ilari/flight-front/src/diaryService.tsx
+++ osa9/example/ilari/flight-front/src/diaryService.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Diary } from "./types";
+import { Diary, NewDiary } from "./types";
 
 const baseUrl = "http://localhost:3001/api/diaries";
 
@@ -7,6 +7,10 @@ const getAllDiaries = () => {
   return axios.get<Diary[]>(baseUrl).then((response) => response.data);
 };
 
-const diaryService = { getAllDiaries };
+const createDiary = (object: NewDiary) => {
+  return axios.post<Diary>(baseUrl, object).then((response) => response.data);
+};
+
+const diaryService = { getAllDiaries, createDiary };
 
 export default diaryService;
